1c42f4a7d03c201c1b785863495fef664e162ccc
womenkuaipao <1576665629@qq.com>
2023-04-23 22:50:45 +0800

    [add] 配置环境

---
+++ src/main/java/human/search/controller/Test.java
@@ -0,0 +1,14 @@
+package human.search.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+    @GetMapping("getString")
+    public String getString(){
+        return "getString";
+    }
+}
